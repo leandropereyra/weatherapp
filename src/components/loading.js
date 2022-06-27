bfd1f77fc33f9a66a168237cdd49dccca1714522
Gastón Leandro Pereyra <leandro-pereyra@hotmail.es>
2022-06-27 14:45:35 +0200

    Weather APP

---
+++ src/components/loading.js
@@ -0,0 +1,8 @@
+import LoadingSpin from "react-loading-spin";
+export default function Loading(){
+    return(
+        <div className="loadingContainer">
+                <div><LoadingSpin /></div>
+        </div>
+    )
+}
